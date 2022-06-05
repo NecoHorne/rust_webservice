45660f8b5ef6086e546dafa163aad6090919891b
Neco Horne <necohorne@gmail.com>
2022-06-05 17:16:48 +0200

    add comments to auth service

--- src/grpc/auth_service.rs
+++ src/grpc/auth_service.rs
@@ -10,6 +10,9 @@ pub struct AuthenticationService {}
 #[tonic::async_trait]
 impl Authentication for AuthenticationService {
 
+    /**
+    * Authenticate a user
+    */
     async fn authenticate_user(&self, request: Request<auth::AuthenticateUserRequest>) -> Result<Response<auth::AuthenticateUserResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -33,6 +36,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Authenticate a service
+     */
     async fn authenticate_service(&self , request: Request<auth::AuthenticateServiceRequest>) -> Result<Response<auth::AuthenticateServiceResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -47,6 +53,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Check if auth token is valid
+     */
     async fn check_token(&self , request: Request<auth::CheckTokenRequest>) -> Result<Response<auth::CheckTokenResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -60,6 +69,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Login user using auth token
+     */
     async fn token_login(&self , request: Request<auth::TokenLoginRequest>) -> Result<Response<auth::CheckTokenResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -73,6 +85,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Refresh the auth token
+     */
     async fn refresh_token(&self , request: Request<auth::RefreshTokenRequest>) -> Result<Response<auth::RefreshTokenResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -87,6 +102,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Get the user details a specific token belongs to
+     */
     async fn get_user_by_token(&self , request: Request<auth::GetUserByTokenRequest>) -> Result<Response<auth::GetsUserByTokenResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -109,6 +127,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Reset a users password
+     */
     async fn reset_password(&self , request: Request<auth::ResetPasswordRequest>) -> Result<Response<auth::ResetPasswordResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -121,6 +142,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Invalidate a users refresh token (used on logouts)
+     */
     async fn invalidate_refresh_token(&self , request: Request<auth::InvalidateRefreshTokenRequest>) -> Result<Response<auth::InvalidateRefreshTokenResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -133,6 +157,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Send a verification email to the user to verify their email
+     */
     async fn verify_user_email(&self , request: Request<auth::VerifyUserEmailRequest>) -> Result<Response<auth::VerifyUserEmailResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -146,6 +173,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * resend the verification email with a token already generated in the database
+     */
     async fn resend_verify_email(&self , request: Request<auth::ResendVerifyEmailRequest>) -> Result<Response<auth::ResendVerifyEmailResponse>, Status>
     {
         println!("Request: {:?}", request);
@@ -159,6 +189,9 @@ impl Authentication for AuthenticationService {
         Ok(Response::new(reply))
     }
 
+    /**
+     * Hash the user password and send back the hash (used for services)
+     */
     async fn hash_password(&self , request: Request<auth::HashPasswordRequest>) -> Result<Response<auth::HashPasswordResponse>, Status>
     {
         println!("Request: {:?}", request);
